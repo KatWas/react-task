4b11841fad9b5d110299428551424399578fade5
Katarzyna Wasilewska <kwasilewska2014@gmail.com>
2022-11-01 20:58:17 +0100

    new option

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,18 +1,24 @@
 import styles from './List.module.scss';
 import Column from '../Column/Column.js';
 
-
 const List = () => {
+
+  const columns = [
+  	{ id: 1, title: 'Books', icon: 'book' },
+    { id: 2, title: 'Movies', icon: 'film' },
+    { id: 3, title: 'Games', icon: 'gamepad' }
+  ];
+  
+
+
     return (
-      <div className = {styles.list}>
+      <div>
           <header className={styles.header}>
               <h2 className={styles.title}>Things to do<span>soon!</span></h2>
           </header>
           <p className={styles.description}>Interesting things I want to check out</p>
           <section className={styles.columns}>
-             ${Column({ title: 'Books'})}<span className="fa fa-book"/>
-             ${Column({ title: 'Movies'})}<span className="fa fa-movie"/>
-             ${Column({ title: 'Games' })}<span className="fa fa-gamepad"/>
+          {columns.map(column => <Column key={column.id} title={column.title} icon={column.icon} />)}
           </section>
       </div>
     );
