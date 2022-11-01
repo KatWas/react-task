82c4a84a13e61aed3e10bb09f0e7b874f17803f0
Katarzyna Wasilewska <kwasilewska2014@gmail.com>
2022-11-01 21:05:44 +0100

    listuse

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,17 +1,18 @@
 import styles from './List.module.scss';
 import Column from '../Column/Column.js';
+import { useState } from 'react';
 
 const List = () => {
-
-  const columns = [
-  	{ id: 1, title: 'Books', icon: 'book' },
-    { id: 2, title: 'Movies', icon: 'film' },
-    { id: 3, title: 'Games', icon: 'gamepad' }
-  ];
-  
-
-
-    return (
+const [columns, setColumns] = useState([
+  { id: 1, title: 'Books', icon: 'book' },
+  { id: 2, title: 'Movies', icon: 'film' },
+  { id: 3, title: 'Games', icon: 'gamepad' }
+]);
+setTimeout(() => {
+  setColumns([...columns, { id: 4, title: 'Test column'}]);
+}, 2000);
+    
+return (
       <div>
           <header className={styles.header}>
               <h2 className={styles.title}>Things to do<span>soon!</span></h2>
