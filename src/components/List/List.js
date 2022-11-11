03440cb8bba0b680d2e3ebaa528b1335f42f6ca5
Katarzyna Wasilewska <kwasilewska2014@gmail.com>
2022-11-11 13:31:45 +0100

    changes in redux

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -3,12 +3,12 @@ import Column from '../Column/Column.js';
 import ColumnForm from '../ColumnForm/ColumnForm';
 import { useSelector } from 'react-redux';
 import { getListById, getColumnsByList } from '../../redux/store';
-import { useParams, Navigate } from 'react-router-dom';
+import { useParams, Navigate } from 'react-router';
 import SearchForm from '../SearchForm/SearchForm';
 
 const List = () => {
   const { listId } = useParams();
-  console.log(listId);
+  //console.log(listId);
   const columns = useSelector((state) => getColumnsByList(state, listId));
 
   const listData = useSelector((state) => getListById(state, listId));
